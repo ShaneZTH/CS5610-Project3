49b7a7af4842ba0e3e69f9c3ceb7769c62982f2f
eileenzhangyf <zhangbbq@bc.edu>
2022-11-10 14:41:57 -0800

    react hooks added

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React,{useState} from 'react'
 //import { BrowserRouter as Router } from 'react-router-dom'
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import '../App.css'
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../style/login.css';
 import save_img from '../images/save.jpg';
 function Login(){
+    var [name,setName]=useState();
+    var [password,setPassword]=useState();
+    const nameUpdate = (event)=>{
+        setName(event.target.value);
+    }
+    const passwordUpdate = (event)=>{
+        setPassword(event.target.value);
+    }
+    const handleSubmit = event =>{
+        console.log(name);
+        console.log(password);
+        alert('You have submitted the form');
+    }
     return(
     <div>
     <div className='login-wrapper'>
         <h2>Log In to See Your Customized Plan</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <ul>
                 <li >
                     <label className='form-label'>Username: </label>
-                    <input type="text"/>
+                    <input type="text" required onChange={nameUpdate}/>
                 </li>
                 <li>
                     <label className='form-label'>Password: </label>
-                    <input type="password"></input>
+                    <input type="password" required onChange={passwordUpdate}/>
                 </li>
             </ul>
             <button type="submit" className='login-button'>Log In</button>
