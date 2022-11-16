a4b9c01154d116e3a243bb3e04c720e872bb703c
eileenzhangyf <zhangbbq@bc.edu>
2022-11-15 16:04:24 -0800

    refresh button added

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -2,30 +2,40 @@ import React,{useState,useEffect} from 'react';
 import '../style/account.css';
 import ExpenseForm from '../components/expenseForm';
 import StatusTable from '../components/statusTable';
+import refresh_img from '../images/refresh.jpeg';
 function Dashboard(props){
-    const [username,setUsername] = useState("");
+    const [username,setUsername] = useState(()=>props.username);
+    const handleclick=()=>{
+        window.location.reload();
+    };
     useEffect(() => {
-       //console.log(window.localStorage.getItem('name'));
-       //setUsername(window.localStorage.getItem('name'));
-        fetch("http://localhost:8080/user",{
-        credentials:'include',
-        method:'GET',
-        headers:{
-            'Content-Type':'application/json',
-        },
-       }).then((res)=>{
-        return res.text();
-       })
-       .then((data)=>{
-            var json_data = JSON.parse(data);
-            setUsername(json_data["user"]);
-    }); 
+/*         fetch("http://localhost:8080/user",{
+            credentials:'include',
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+            },
+            }).then((res)=>{
+            return res.text();
+            })
+            .then((data)=>{
+                console.log(data);
+            //var json_data = JSON.parse(data);
+                if(typeof data === 'string'){
+                    setUsername(data);
+                }
+            }); */
+        console.log(window.localStorage.getItem('name'));
+        setUsername(window.localStorage.getItem('name'));
       }, []); 
     return(
         <div className='dashboard-page'>
             <h2 className='greeting'>Hi {username}, let's start saving today!</h2>
             <ExpenseForm curr_user={username}/>
-            <h2 className='status-text'>Current Status</h2>
+            <div className='status-div'>
+                <h2 className='status-text'>Current Status</h2>
+                <button className='refresh-button' onClick={handleclick}><img src={refresh_img} alt='' className='refresh-img'/></button>
+            </div>
             <StatusTable className='curr-table'/>
         </div>
     );
