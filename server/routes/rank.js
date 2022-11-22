d99139c1a4d1ac7f27bbc6aa85e4dcf5064107d1
Shane Z <22213632+ShaneZTH@users.noreply.github.com>
2022-11-22 15:25:02 -0800

    WIP ranking

--- server/routes/rank.js
+++ server/routes/rank.js
@@ -1,17 +1,21 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-const mongoUtil = require('../db/mongoUtil.js');
+const mongoUtil = require("../db/mongoUtil.js");
 const db = mongoUtil.getDb();
 
-router.post("/",function(req,res){
-    var collection = 'ranktest';
-    var query = {'username':req.body.username};
-    db.collection(collection).findOneAndReplace(query,req.body,{upsert:true},function(err,doc){
-        if (err) return res.send(500, {error: err});
-        return res.status(204).send();
-    });
-    
+router.post("/", function (req, res) {
+  var collection = "ranktest";
+  var query = { username: req.body.username };
+  db.collection(collection).findOneAndReplace(
+    query,
+    req.body,
+    { upsert: true },
+    function (err, doc) {
+      if (err) return res.send(500, { error: err });
+      return res.status(204).send();
+    }
+  );
 });
 
 module.exports = router;
