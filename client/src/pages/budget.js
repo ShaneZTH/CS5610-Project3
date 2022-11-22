0d9f181662c7e3bdbb8dd9469990c072514b4351
eileenzhangyf <zhangbbq@bc.edu>
2022-11-22 14:45:44 -0800

    budget page done

--- client/src/pages/budget.js
+++ client/src/pages/budget.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ExpenseForm from "../components/expenseForm";
 import BudgetForm from "../components/budgetForm";
+import "../style/budget.css";
 function Budget() {
   const [username, setUsername] = useState("");
   useEffect(() => {
@@ -8,7 +9,7 @@ function Budget() {
     setUsername(window.localStorage.getItem("name"));
   }, []);
   return (
-    <div>
+    <div className="budget-page">
       <h2 className="greeting">Hi {username}, lets start saving today!</h2>
       <ExpenseForm curr_user={username} />
       <BudgetForm />
