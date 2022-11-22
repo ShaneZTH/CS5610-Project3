8d2bd6495cc7dcb8e53b50d4917bbffc3115de78
eileenzhangyf <zhangbbq@bc.edu>
2022-11-22 11:35:55 -0800

    small change

--- server/app.js
+++ server/app.js
@@ -55,7 +55,6 @@ app.use(passport.session());
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-=======
 
 app.use(express.urlencoded({ extended: true })); // TODO: do we need true / false here?
 require("dotenv").config();
