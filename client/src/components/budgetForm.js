da232cd74b0577d8503a6648c1b8111599b79c30
eileenzhangyf <zhangbbq@bc.edu>
2022-11-22 14:37:02 -0800

    formatted with prettier

---
+++ client/src/components/budgetForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../style/expense.css";
+
+function BudgetForm() {
+  const navigate = useNavigate();
+  var [Category, setCategory] = useState();
+  var [amount, setAmount] = useState();
+  const updateCategory = (e) => {
+    setCategory(e.target.value);
+    // console.log(Category);
+  };
+  const updateAmount = (e) => {
+    setAmount(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(Category);
+    console.log(amount);
+    const postURL = "http://localhost:8080/budget";
+    fetch(postURL, {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        category: Category,
+        amount: amount,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          console.log(res);
+          alert("Log in first to proceed");
+          navigate("/");
+          return new Error(res.statusText);
+        }
+        const string = res.text();
+        console.log(res);
+        navigate("/account");
+        window.location.reload();
+        return string;
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <div className="expense-form">
+        <h3>Record your monthly budget</h3>
+        <form onSubmit={handleSubmit}>
+          <ul>
+            <li>
+              <label className="dropdown-label">
+                Category:
+                {/* <input type='text' required></input> */}
+                <select
+                  value={Category}
+                  onChange={updateCategory}
+                  required
+                  className="dropdown-select"
+                >
+                  <option value="none">None</option>
+                  <option value="dining">Dining</option>
+                  <option value="grocery">Grocery</option>
+                  <option value="entertainment">Entertainment</option>
+                  <option value="clothes">Clothes & Makeup</option>
+                  <option value="travel">Traveling</option>
+                  <option value="medicene">Medication</option>
+                  <option value="others">Others</option>
+                </select>
+              </label>
+            </li>
+            <li>
+              <label className="expense-form-label">Amount:</label>
+              <input type="text" required onChange={updateAmount}></input>
+            </li>
+          </ul>
+          <button type="submit" className="save-button">
+            Save
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+export default BudgetForm;
--- client/src/pages/budget.js
+++ client/src/pages/budget.js
@@ -1,17 +1,19 @@
-import React,{useState,useEffect} from 'react';
-import ExpenseForm from '../components/expenseForm';
-function Budget(){
-    const [username,setUsername] = useState("");
-    useEffect(() => {
-        console.log(window.localStorage.getItem('name'));
-        setUsername(window.localStorage.getItem('name'));
-    }, []); 
-    return(
-        <div>
-            <h2 className='greeting'>Hi {username}, let's start saving today!</h2>
-            <ExpenseForm curr_user={username}/>
-        </div>
-    );
+import React, { useState, useEffect } from "react";
+import ExpenseForm from "../components/expenseForm";
+import BudgetForm from "../components/budgetForm";
+function Budget() {
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    console.log(window.localStorage.getItem("name"));
+    setUsername(window.localStorage.getItem("name"));
+  }, []);
+  return (
+    <div>
+      <h2 className="greeting">Hi {username}, lets start saving today!</h2>
+      <ExpenseForm curr_user={username} />
+      <BudgetForm />
+    </div>
+  );
 }
 
 export default Budget;
