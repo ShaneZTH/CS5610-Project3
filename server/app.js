8a33e0a557149b6efc078a03f47764524206c302
eileenzhangyf <zhangbbq@bc.edu>
2022-11-28 13:34:57 -0800

    app js updated

--- server/app.js
+++ server/app.js
@@ -20,7 +20,12 @@ var corsOptions = {
   methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE"],
   credentials: true,
 };
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
 
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors(corsOptions));
 app.use(cookieParser());
