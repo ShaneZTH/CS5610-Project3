46d6ea5ed46497c47ba5ebe07e8a4f8540c5c374
Shane Z <22213632+ShaneZTH@users.noreply.github.com>
2022-12-05 00:57:49 -0800

    Fix deployment issues WIP

--- server/app.js
+++ server/app.js
@@ -57,7 +57,7 @@ app.get("/", (req, res) => {
 ////////////////////////////////////
 // Routing
 ////////////////////////////////////
-app.listen(process.env.PORT || PORT, () => {
+app.listen(process.env.PORT || PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
